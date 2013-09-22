e99ced9b6cd02dd805e730dfe7a0062ff8cb6c04
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2013-09-22 20:56:27 +0530

    helpers updated

--- js/base/helpers.js
+++ js/base/helpers.js
@@ -1,7 +1,7 @@
 define(['base/app'], function(app) {
 
     Handlebars.registerHelper('elementLabel', function(element) {
-        return element.name;
+        return element.label || element.name;
     });
 
     Handlebars.registerHelper('stringify', function(obj) {
@@ -14,6 +14,18 @@ define(['base/app'], function(app) {
             return attributeName;
         }
     });
+
+    Handlebars.registerHelper('ifEqual', function(val1, val2, obj) {
+
+        if (val1 === val2) {
+            return obj.fn(this);
+        }
+        else if (obj.inverse) {
+            return obj.inverse(this);
+        }
+    });
+
+
 
 
 });
